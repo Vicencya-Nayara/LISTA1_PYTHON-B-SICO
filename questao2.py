3c35da86a3ee41be703c85cb0a6860b2a87c770c
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-04-04 23:25:35 -0300

    Delete questao2.py

--- questao2.py
+++
@@ -1,7 +0,0 @@
-#Crie um programa que leia dois números do usuário e exiba a soma desses números.
-
-num1 = int(input('Digite o primeiro numero: '))
-num2 = int(input('Digite  o segundo numero: '))
-
-soma = num1 + num2
-print('A soma é: ',soma)
