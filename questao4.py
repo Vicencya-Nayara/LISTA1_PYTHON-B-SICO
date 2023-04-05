8c441104e8893faa7dd566bd161a28d37c07d6c9
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-04-04 23:26:24 -0300

    Delete questao4.py

--- questao4.py
+++
@@ -1,4 +0,0 @@
-#Crie um programa que leia uma palavra do usuário e exiba a primeira letra dessa palavra.
-
-palavra = str(input('Escreva o nome do usuário: '))
-print('Primeira letra: ', palavra[0])
