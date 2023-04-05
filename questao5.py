9277db6c5dd7b56b3c9c5d57a5e63fec5ec5e072
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-04-04 23:26:36 -0300

    Delete questao5.py

--- questao5.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que leia um número do usuário e determine se esse número é par ou ímpar.
-
-numero = int(input('Digite o número: '))
-
-if numero %2==0:
-    print('O número é par')
-else:
-    print('O número é ímpar')
