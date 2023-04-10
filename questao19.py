11084794862cae4ce099e7e8d4b9298608c426e8
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-04-10 16:39:40 -0300

    Update questao19.py

--- questao19.py
+++ questao19.py
@@ -2,9 +2,9 @@
 
 numeros = []
 
-for i in range(0,5):
+for i in range(0, 5):
     numero = int(input('Digite um número: '))
     numeros.append(numero)
 
-numeros_crescente = sorted(numeros)
-print('Os números em ordem crescente são: ', numeros_crescente)
+numeros.sort()
+print('Os números em ordem crescente são:', numeros)
